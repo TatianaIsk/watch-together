b47cdb5324c8d000e033766cbed98d9a11ca18b5
Татьяна <isskova.tatyana@yandex.ru>
2024-01-08 16:36:35 +0300

    feat: user count definition functionality

---
+++ app/room/components/UsersBlock/Users/index.tsx
@@ -0,0 +1,7 @@
+import user from '@/assets/user.svg';
+
+const User = () => {
+	return (
+		<div>index</div>
+	)
+}
--- app/room/components/UsersBlock/index.tsx
+++ app/room/components/UsersBlock/index.tsx
@@ -1,3 +1,48 @@
-const UsersBlock = () => <div></div>;
+'use client';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import Image from 'next/image';
+import user from '@/assets/user.svg';
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const UsersBlock: React.FC = () => {
+  const [userCount, setUserCount] = useState<number>(1);
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+
+    socket.on('userCountUpdated', (count: number) => {
+      setUserCount(count);
+    });
+
+    socket.on('usersUpdated', (data: User[]) => {
+      setUsers(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const userBlocks = [];
+  for (let i = 0; i < userCount; i++) {
+    userBlocks.push(
+      <div key={i}>
+        {users.map((user) => (
+          <div key={user.id}>
+            {user.name}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return <div>{userBlocks}</div>;
+};
 
 export default UsersBlock;
